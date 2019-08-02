e8e5c4ec7babcebb443340f2ff4144cb72c5a0f1
hermanomark <hermano.mark.mh@gmail.com>
2019-08-02 19:53:36 +0800

    done vidjot ready for deployment

--- projects/vidjot/app.js
+++ projects/vidjot/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
 const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // How middleware works
 // app.use(function(req, res, next) {
@@ -22,10 +34,6 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
 // Handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -33,6 +41,35 @@ app.set('view engine', 'handlebars');
 // Body parser moddileware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method override middleware
+app.use(methodOverride('_method'));
+
+// Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport middleware (must be after session middleware)
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect-flash middleware
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
 // Index Route
 app.get('/', (req, res) => {
@@ -50,55 +87,9 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({date:'desc'})
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas:ideas
-            });
-        });
-});
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
-
-// Process Form
-app.post('/ideas', (req, res) => {
-    // console.log(req.body);
-    // res.send('ok');
-    let errors = [];
-
-    if (!req.body.title) {
-        errors.push({text: 'Please add a title'});
-    }
-    if (!req.body.details) {
-        errors.push({text: 'Please add some details'});
-    }
-
-    if(errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        // res.send('passed');
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        }
-
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                res.redirect('/ideas');
-            });
-    }
-});
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
