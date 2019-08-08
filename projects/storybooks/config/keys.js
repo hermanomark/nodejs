15141305a4efddbc33826057d63ba5e920a36f9a
hermanomark <hermano.mark.mh@gmail.com>
2019-08-08 19:21:18 +0800

    fixing commits

---
+++ projects/storybooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://hermanomark:mark@storybooks-dev-faosq.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID: '117437131133-ss8564gvih1ehcq2d646imoqcb71havu.apps.googleusercontent.com',
+    googleClientSecret: '2ybsOKxT_oFDTW_bV88UJPOl'
+}
