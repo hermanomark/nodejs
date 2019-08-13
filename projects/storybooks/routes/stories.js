a3a80285d2a25773e885e44eca5786f13e186d37
hermanomark <hermano.mark.mh@gmail.com>
2019-08-13 17:02:39 +0800

    created ui storybooks

--- projects/storybooks/routes/stories.js
+++ projects/storybooks/routes/stories.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 router.get('/', (req, res) => {
     res.render('stories/index');
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
 });
 
-router.get('/edit', (req, res) => {
+router.get('/edit', ensureAuthenticated, (req, res) => {
     res.render('stories/edit');
 });
 
-router.get('/show', (req, res) => {
+router.get('/show', ensureAuthenticated, (req, res) => {
     res.render('stories/show');
 });
 
