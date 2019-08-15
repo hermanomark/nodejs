56122f4128fbf59419ae4bd1adad55a5184c99bf
hermanomark <hermano.mark.mh@gmail.com>
2019-08-15 17:10:59 +0800

    finished storybooks

--- projects/storybooks/app.js
+++ projects/storybooks/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -27,7 +28,8 @@ const {
     truncate,
     stripTags,
     formatDate,
-    select
+    select,
+    editIcon
 } = require('./helpers/hbs');
 
 // Map global promises
@@ -46,13 +48,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
     helpers: {
         truncate: truncate,
         stripTags, stripTags,
         formatDate: formatDate,
-        select: select
+        select: select,
+        editIcon: editIcon
     },
     defaultLayout: 'main'
 }));
--- projects/storybooks/config/keys.js
+++ projects/storybooks/config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://hermanomark:mark@storybooks-dev-faosq.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientID: '117437131133-ss8564gvih1ehcq2d646imoqcb71havu.apps.googleusercontent.com',
-    googleClientSecret: '2ybsOKxT_oFDTW_bV88UJPOl'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
